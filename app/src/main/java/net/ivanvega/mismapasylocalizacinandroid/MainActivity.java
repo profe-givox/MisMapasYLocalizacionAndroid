42003d8bbe61a8da1bccf49b589cad513af299b0
profe-givox <gi.vega@itsur.edu.mx>
2021-03-23 14:48:59 -0600

    sE AGREGO UBICACION ULTIMA CONOCIDA. NO SE VALIDAN PERMISOS EN TIEMPO DE EJECUCION NI TAMPOCO FORZA A REQ DE UBICACION

--- app/src/main/java/net/ivanvega/mismapasylocalizacinandroid/MainActivity.java
+++ app/src/main/java/net/ivanvega/mismapasylocalizacinandroid/MainActivity.java
@@ -1,9 +1,22 @@
 package net.ivanvega.mismapasylocalizacinandroid;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,12 +25,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
+
     SupportMapFragment mapFragment;
     GoogleMap mapa;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +59,14 @@ public class MainActivity extends AppCompatActivity
                 .title("Marker"));
 
 
-
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"));
 
-        CameraUpdate  cameraUpdate = CameraUpdateFactory.newLatLng(sydney);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(sydney);
 
         mapa.animateCamera(cameraUpdate);
-
-
 
 
         CameraPosition cameraPosition = CameraPosition.builder()
@@ -65,8 +80,56 @@ public class MainActivity extends AppCompatActivity
         mapa.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
 
-        mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //mapa.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
+        // Add polylines to the map.
+        // Polylines are useful to show a route or some other connection between points.
+        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(-35.016, 143.321),
+                        new LatLng(-34.747, 145.592),
+                        new LatLng(-34.364, 147.891),
+                        new LatLng(-33.501, 150.217),
+                        new LatLng(-32.306, 149.248),
+                        new LatLng(-32.491, 147.309)));
+
+        // Position the map's camera near Alice Springs in the center of Australia,
+        // and set the zoom factor so most of Australia shows on the screen.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-23.684, 133.903), 4));
+
+
+        clienteFusedLocation();
+
+    }
+
+    private void clienteFusedLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            Log.d("MIXUBICA", "Lat: " + location.getLatitude() +
+                                    " , lon" + location.getLongitude());
+                            Toast.makeText(getBaseContext(), "Lat: " + location.getLatitude() +
+                                    " , lon" + location.getLongitude(),Toast.LENGTH_LONG )
+                                    .show();
+                        }
+                    }
+                });
     }
 
 
